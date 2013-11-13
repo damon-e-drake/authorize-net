c4da76648df2e0d838ea64966dc1c6eb99cd8823
Damon E. Drake <damon.e.drake@gmail.com>
2013-11-13 12:24:18 -0600

    Added in functionality for Unsettled Transactions Detail.

---
+++ TransactionDetails/TransactionDetailSummary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace AuthorizeNetLite.TransactionDetails {
+  [Serializable]
+  [XmlRoot("transaction")]
+  public class TransactionDetailSummary {
+    [XmlElement("transId")]
+    public long TransactionID { get; set; }
+    [XmlElement("submitTimeUTC")]
+    public DateTime SubmittedUTC { get; set; }
+    [XmlElement("submitTimeLocal")]
+    public DateTime SubmittedLocal { get; set; }
+    [XmlElement("invoiceNumber")]
+    public string InvoiceNumber { get; set; }
+    [XmlElement("firstName")]
+    public string FirstName { get; set; }
+    [XmlElement("lastName")]
+    public string LastName { get; set; }
+    [XmlElement("accountType")]
+    public string AccountType { get; set; }
+    [XmlElement("accountNumber")]
+    public string AccountNumber { get; set; }
+    [XmlElement("settleAmount")]
+    public decimal SettlementAmount { get; set; }
+  }
+}
---
+++ TransactionDetails/UnsettledResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Xml.Serialization;
+using AuthorizeNetLite.Response;
+
+namespace AuthorizeNetLite.TransactionDetails {
+  [Serializable]
+  [XmlRoot("getUnsettledTransactionListResponse", Namespace = "AnetApi/xml/v1/schema/AnetApiSchema.xsd")]
+  public class UnsettledResponse {
+    [XmlElement("messages")]
+    public Status Status { get; set; }
+
+    [XmlArray("transactions")]
+    [XmlArrayItem("transaction", typeof(TransactionDetailSummary))]
+    public TransactionDetailSummary[] Transactions { get; set; }
+  }
+}
