3a1a7ffff02e87a0da418c4b7a6dc1b8823efb29
Damon E. Drake <damon.e.drake@gmail.com>
2017-03-21 11:47:08 -0500

    Cleaned up service to use overloads. Also added functionality for raw json requests and the ability to add other request / response types using the interface.

--- AuthorizeNetService.cs
+++ AuthorizeNetService.cs
@@ -11,32 +11,52 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace AuthorizeNetLite {
-  public class AuthorizeNetService {
+  public class AuthorizeNetService : IDisposable {
     private HttpClient _client { get; set; }
     private Authentication _credentials { get; set; }
     private string _endpoint { get; set; }
+    private bool _disposed { get; set; }
 
+    /// <summary>
+    /// This creates an instance of the base AuthorizeNet service. This is meant to be used in a static context for an entire
+    /// application.
+    /// </summary>
+    /// <param name="credentials">API login credentials</param>
+    /// <param name="endpoint">Optional - Defaults to Production</param>
     public AuthorizeNetService(Authentication credentials, ApiEndpoint endpoint = ApiEndpoint.Production) {
       _client = new HttpClient();
       _credentials = credentials;
       _endpoint = endpoint == ApiEndpoint.Sandbox ? "https://apitest.authorize.net/xml/v1/request.api" : "https://api.authorize.net/xml/v1/request.api";
+      _disposed = false;
     }
 
-    public async Task<Response> ExecuteRequestAsync<Request, Response>(Request obj) {
-      var json = await ExecuteRequestRawAsync(obj);
+    public string GenerateRequestJson<T>(T obj, Formatting format = Formatting.None) {
+      if (!typeof(IAuthorizeNetRequest).IsAssignableFrom(typeof(T))) { throw new Exception("Can't create a request from type: " + typeof(T).ToString()); }
+
+      ((IAuthorizeNetRequest)obj).Credentials = _credentials;
+      return "{\"" + GetApiName<T>() + "\":" + JsonConvert.SerializeObject(obj, format) + "}";
+    }
+
+    public async Task<string> ExecuteAsync(string json, CancellationToken token = default(CancellationToken)) {
+      using (var response = await _client.PostAsync(_endpoint, new StringContent(json, Encoding.UTF8, "application/json"), token).ConfigureAwait(false)) {
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+      }
+    }
+
+    public async Task<Response> ExecuteAsync<Request, Response>(Request obj) {
+      var json = await ExecuteAsync(obj);
+      if (!typeof(IAuthorizeNetResponse).IsAssignableFrom(typeof(Response))) { throw new Exception(typeof(Response).ToString() + " does not implement IAuthorizeNetResponse."); }
       return default(Response);
     }
 
-    public async Task<string> ExecuteRequestRawAsync<T>(T obj, CancellationToken token = default(CancellationToken)) {
+    public async Task<string> ExecuteAsync<T>(T obj, CancellationToken token = default(CancellationToken)) {
       if (!typeof(IAuthorizeNetRequest).IsAssignableFrom(typeof(T))) { throw new Exception("Can't create a request from type: " + typeof(T).ToString()); }
 
       ((IAuthorizeNetRequest)obj).Credentials = _credentials;
       var json = "{\"" + GetApiName<T>() + "\":" + JsonConvert.SerializeObject(obj) + "}";
 
-      using (var response = await _client.PostAsync(_endpoint, new StringContent(json, Encoding.UTF8, "application/json"), token).ConfigureAwait(false)) {
-        response.EnsureSuccessStatusCode();
-        return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-      }
+      return await ExecuteAsync(json);
     }
 
     private string GetApiName<T>() {
@@ -44,6 +64,27 @@ namespace AuthorizeNetLite {
       if (attr != null) { return attr.Name; }
 
       return typeof(T).Name.ToLower();
+    }
+
+    protected virtual void Dispose(bool disposing) {
+      if (!_disposed) {
+        if (disposing) {
+          _credentials = null;
+          _endpoint = null;
+        }
+
+        _client.Dispose();
+        _disposed = true;
+      }
+    }
+
+    ~AuthorizeNetService() {
+      Dispose(false);
+    }
+
+    public void Dispose() {
+      Dispose(true);
+      GC.SuppressFinalize(this);
     }
   }
 }
---
+++ Interfaces/IAuthorizeNetResponse.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizeNetLite.Interfaces {
+
+  public sealed class ResponseMessage {
+    [JsonProperty("code", Order = 1)]
+    public string Code { get; set; }
+    [JsonProperty("description", Order = 2)]
+    public string Description { get; set; }
+  }
+
+  public sealed class ResponseError {
+    [JsonProperty("errorCode", Order = 1)]
+    public string Code { get; set; }
+    [JsonProperty("errorText", Order = 2)]
+    public string Description { get; set; }
+  }
+
+  public interface IAuthorizeNetResponse {
+    List<ResponseMessage> Messages { get; set; }
+  }
+
+  public sealed class TransactionResponse : IAuthorizeNetResponse {
+    [JsonProperty("responseCode")]
+    public string ResponseCode { get; set; }
+    [JsonProperty("rawResponseCode")]
+    public string RawResponseCode { get; set; }
+    [JsonProperty("authCode")]
+    public string AuthorizationCode { get; set; }
+    [JsonProperty("avsResultCode")]
+    public string AvsResultCode { get; set;}
+    [JsonProperty("cvvResultCode")]
+    public string CvvResultCode { get; set; }
+    [JsonProperty("cavvResultCode")]
+    public string CavvResultCode { get; set; }
+
+
+    public List<ResponseMessage> Messages { get; set; }
+
+  }
+}
